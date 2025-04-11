e2357711dab95a2bd2eada971d778c704d2f0327
Ayush Mishra <a06mishra@gmail.com>
2025-04-11 23:54:00 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 // basic task is to create a wrapper function
 const asyncHandler=(requestHandler)=>{
-    (req,res,next)=>{
+    return (req,res,next)=>{
         Promise.resolve(requestHandler(req,res,next)).catch((err)=> next(err))
     }
 
